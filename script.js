f9d7abb0a6655f16e52103bf3dcddfddc7a6e966
alberto londono <albertolondono.londono5@gmail.com>
2022-05-06 17:30:48 -0500

    index.html is still the same. added code to get item from local storage but does not stay when page is refreshed. it still stays in the local storage. getting a cookie error on my local end when trying to test locally. Going to try remotely and see how it works on a deployed site.

--- script.js
+++ script.js
@@ -10,5 +10,17 @@ $(document).ready(function () {
         let time = $(this).parent().attr("id");
 
         localStorage.setItem(time, text);
-    });
+    })
+
+    // retrieves what is stored in the local storage for the specified hour
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
     })

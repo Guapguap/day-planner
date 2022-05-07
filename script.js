8d08e70e17632952ef22b3e71f37b41b5aa03c51
alberto londono <albertolondono.londono5@gmail.com>
2022-05-07 18:45:07 -0500

    added an extra timeblock for an even 10. Styled the layout a bit by adding some color. Added a clear button to clear the local storage and refresh the page

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // these variables display the current date in the header
-var currentDay = moment().format('dddd, MMM Do YYYY');
-var timeNow = moment().hour();
+let currentDay = moment().format('dddd, MMM Do YYYY');
+let timeNow = moment().hour();
 $("#currentDay").html(currentDay);
+
+// this variable links to the clear button 
+let clearBtn = $('#clearButton');
+
+// button to clear and reset the localStorage 
+clearBtn.on("click", function () {
+    localStorage.clear();
+    location.reload();
+});
 
 // Once the save button is clicked, it will set the time and text in local storage.
 $(document).ready(function () {
@@ -22,5 +31,5 @@ $(document).ready(function () {
     $("#hour-15 .description").val(localStorage.getItem("hour-15"));
     $("#hour-16 .description").val(localStorage.getItem("hour-16"));
     $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-
-    })
+    $("#hour-18 .description").val(localStorage.getItem("hour-18"));
+})
